912dca1c819d5719714fed1748e163471be1a6a3
Pavel Krymets <pakrym@stripe.com>
2022-02-17 14:43:09 -0800

    Some feedback changes

--- create-cardholders-and-cards/server/dotnet/Models/CreateCardholderRequest.cs
+++ create-cardholders-and-cards/server/dotnet/Models/CreateCardholderRequest.cs
@@ -1,29 +1,19 @@
-using System.Text.Json;
 using System.Text.Json.Serialization;
 
-public class CreateCardholderRequest
-{
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
-
-    [JsonPropertyName("email")]
-    public string Email { get; set; }
-
-    [JsonPropertyName("phone_number")]
-    public string PhoneNumber { get; set; }
-
-    [JsonPropertyName("line1")]
-    public string Line1 { get; set; }
-
-    [JsonPropertyName("state")]
-    public string State { get; set; }
-
-    [JsonPropertyName("city")]
-    public string City { get; set; }
-
-    [JsonPropertyName("postal_code")]
-    public string PostalCode { get; set; }
-
-    [JsonPropertyName("country")]
-    public string Country { get; set; }
-}
+public record CreateCardholderRequest(
+    [property: JsonPropertyName("name")]
+    string Name,
+    [property: JsonPropertyName("email")]
+    string Email,
+    [property: JsonPropertyName("phone_number")]
+    string PhoneNumber,
+    [property: JsonPropertyName("line1")]
+    string Line1,
+    [property: JsonPropertyName("state")]
+    string State,
+    [property: JsonPropertyName("city")]
+    string City,
+    [property: JsonPropertyName("postal_code")]
+    string PostalCode,
+    [property: JsonPropertyName("country")]
+    string Country);
